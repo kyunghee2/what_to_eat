282bda9f32c618c3b7821443c2a9d0b3b854e393
dohyun <ehgus1551@naver.com>
2019-11-19 20:10:39 +0900

    views.py csv filesave 주석

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-import csv
-from .models import Restaurant
+# import csv
+# from .models import Restaurant
+
+
 # Create your views here.
 def index(request):
     return render(request, 'restaurant/index.html' )
