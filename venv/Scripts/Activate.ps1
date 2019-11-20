a4f0eee8df4de59b1f4eb9a8db267d38486342fa
kyunghee2 <kyungh103@gmail.com>
2019-11-20 12:59:40 +0900

    merge

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\what_to_eat\venv"
+$env:VIRTUAL_ENV="C:\Users\student\Desktop\what_to_eat\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
