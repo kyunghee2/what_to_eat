1be31a2d0e9ace4b8b1a5a820a614ab69ac61a88
YeongSeonKim <son960311@naver.com>
2019-11-22 11:16:59 +0900

    query->search로 변경, 검색창변경, 음식점화면 수정중

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -7,5 +7,5 @@ app_name = 'restaurant'
 urlpatterns = [
    path('', views.index, name='index'),
    # path('csvfilesave/',views.csvfilesave, name='csvfilesave'),
-   path('query/', views.query, name='query'),
+   path('search/', views.search, name='search'),
 ]
