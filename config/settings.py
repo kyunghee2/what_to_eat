7cab7b8aca4aa95900b78f32eba634869ce7a00a
kyunghee2 <kyungh103@gmail.com>
2019-11-25 11:20:02 +0900

    heroku 설정

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fxfugd7f$(!os0yrnogui$xfh@=*(6b!(0&v(k=y5&wag4zq04'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
@@ -156,3 +157,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # CRONJOBS = [
 #     ('*/1 * * * *', 'chatbot.cron.my_cron_job')
 # ]
+
+#heroku settings
+import django_heroku
+django_heroku(locals())
