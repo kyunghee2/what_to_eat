1167958c4e9e2b49b6de7b27b1c5b550589ab1e4
kyunghee2 <kyungh103@gmail.com>
2019-11-26 15:16:06 +0900

    db연결정보 수정

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
    path('comments/<int:restaurant_pk>', views.comments_create, name='comment'),
    path('how_to_use_chatbot/', views.how_to_use_chatbot, name='how_to_use_chatbot'),
    path('people/<int:people>/', views.people, name='people'),
-   path('csvfilesave/',views.csvfilesave,name='csvfilesave'),
+   # path('csvfilesave/',views.csvfilesave,name='csvfilesave'),
 ]
