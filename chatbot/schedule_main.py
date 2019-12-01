d8d85453cec2c9dbc65913c5b84adc07461c6012
kyungh103 <kyungh103@gmail.com>
2019-12-01 21:12:50 +0900

    요일체크작업

--- chatbot/schedule_main.py
+++ chatbot/schedule_main.py
@@ -1,8 +1,12 @@
 import requests 
-  
-URL = "https://sleepy-thicket-13178.herokuapp.com/chatbot/telegram_chat_schedule/"
-  
-PARAMS = {}    
-r = requests.get(url = URL, params = PARAMS) 
+import datetime
 
-print(r)
+URL = "https://sleepy-thicket-13178.herokuapp.com/chatbot/telegram_chat_schedule/"
+
+t = ['월','화','수','목','금','토','일']
+r = datetime.datetime.today().weekday()
+print(t[r])
+# PARAMS = {}    
+# r = requests.get(url = URL, params = PARAMS) 
+
+# print(r)
