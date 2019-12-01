c24fe0e0ace7bbe96540a6d119e104d7e9eeb252
kyungh103 <kyungh103@gmail.com>
2019-12-01 21:46:05 +0900

    요일체크 로직추가

--- chatbot/schedule_main.py
+++ chatbot/schedule_main.py
@@ -1,12 +1,17 @@
 import requests 
 import datetime
+from decouple import config
 
-URL = "https://sleepy-thicket-13178.herokuapp.com/chatbot/telegram_chat_schedule/"
+schedule_url = config('SCHEDULE_URL')
 
 t = ['월','화','수','목','금','토','일']
 r = datetime.datetime.today().weekday()
-print(t[r])
-# PARAMS = {}    
-# r = requests.get(url = URL, params = PARAMS) 
-
-# print(r)
+week = t[r]
+print(week)
+if week != "일" and week != "토":    
+    PARAMS = {}    
+    r = requests.get(url = schedule_url, params = PARAMS) 
+    print(">>>>")
+    print(r)
+else:
+    pass
